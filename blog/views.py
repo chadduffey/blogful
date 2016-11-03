70a379776caf5f1cd4788067a7a7c034425a9001
Chad Duffey <chadduffey@hotmail.com>
2016-11-02 19:37:27 -0700

    pagination links for the entries page

--- blog/views.py
+++ blog/views.py
@@ -16,13 +16,14 @@ def entries(page=1):
     start = page_index * PAGINATE_BY
     end = start + PAGINATE_BY
 
-    total_pages = (count - 1) // PAGINATE_BY + 1
+    total_pages = (count - 1) / PAGINATE_BY + 1
     has_next = page_index < total_pages - 1
     has_prev = page_index > 0
 
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
-    entries = entries.all()
+    entries = entries[start:end]
+
     return render_template("entries.html",
                            entries=entries,
                            has_next=has_next,
