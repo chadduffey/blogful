5ede7e8d30a946dc06af13788403344abe6918c1
Chad Duffey <chadduffey@hotmail.com>
2016-11-05 17:04:53 -0700

    login route implemented

--- blog/database.py
+++ blog/database.py
@@ -1,5 +1,7 @@
 import datetime
 
+from flask.ext.login import UserMixin
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -19,6 +21,16 @@ class Entry(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password=Column(String(128))
+
 
 Base.metadata.create_all(engine)
 
